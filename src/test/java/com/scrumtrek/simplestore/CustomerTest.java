a8b7108ffd4b373f1b612d4681c4b388519348ea
grasdy <ggryazno@gmail.com>
2017-08-07 18:20:34 +0300

    Customer Test

---
+++ src/test/java/com/scrumtrek/simplestore/CustomerTest.java
@@ -0,0 +1,39 @@
+package com.scrumtrek.simplestore;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class CustomerTest {
+
+    @Test
+    public void shouldNameBeTest(){
+
+        Customer sut = new Customer("Test" );
+        String name = sut.getName();
+        Assert.assertEquals("Test", name);
+    }
+
+    @Test
+    public void shouldContainRentalRecords(){
+
+        Customer sut = new Customer("Test");
+        String statement = sut.Statement();
+        Assert.assertTrue(statement.contains("Rental record for Test"));
+        Assert.assertTrue(statement.contains("Amount owed is "));
+        Assert.assertTrue(statement.contains("You earned "));
+        Assert.assertTrue(statement.contains(" frequent renter points."));
+    }
+
+    @Test
+    public void shouldContainNewReleaseStatus(){
+        Customer sut = new Customer("Test");
+        Movie movie = new Movie("NewRelease", PriceCodes.NewRelease);
+        Rental rental = new Rental(movie, 2);
+
+        sut.addRental(rental);
+
+        String statement = sut.Statement();
+        Assert.assertTrue(statement.contains("\tNewRelease\t"));
+    }
+}
